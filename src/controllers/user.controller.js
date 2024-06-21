c1cfc4e47f9490d362a1eb7194b2ae9bc1d2d9a1
Abinash <dashabinash2002@gmail.com>
2024-06-21 20:50:40 +0530

    postman testing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,44 +15,44 @@ const registerUser = asyncHandler( async (req,res) => {
     // check for user creation
     // return res
     
-    const { username, email, fullname, password} = req.body
-    console.log("email",email)
-    
-    if ( 
-        [username,email,fullname,password].some( (field) => field?.trim() === "" )
-    ){
-        throw new ApiError(400, "All field is required")
+    const {fullName, email, username, password } = req.body
+    //console.log("email: ", email);
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
-        $or: [{username},{email}]
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
 
     if (existedUser) {
-        throw new ApiError(409,"User with username or email already exists")
+        throw new ApiError(409, "User with email or username already exists")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-    if (!avatarLocalPath){
-        throw new ApiError(400, "Avatar field is required")
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if (!avatar){
-        throw new ApiError(400, "Avatar field is required")
+    if (!avatar) {
+        throw new ApiError(400, "Avatar file is required")
     }
 
     const user = await User.create({
-        username: username.toLowerCase(),
-        fullname,
-        email,
-        password,
+        fullName,
         avatar: avatar.url,
-        coverImage: coverImage?.url || ""
+        coverImage: coverImage?.url || "",
+        email, 
+        password,
+        username: username.toLowerCase()
     })
 
     const createdUser = await User.findById(user._id).select( "-password -refreshToken" )
@@ -66,5 +66,5 @@ const registerUser = asyncHandler( async (req,res) => {
     )
 
 })
-
+ 
 export {registerUser}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
         trim: true,
         index: true
     },
-    avtar : {
+    avatar : {
         type: String, //cloudinary url
         required: true,
     },
