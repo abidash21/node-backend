9410d99c05bfecc05d2237fdded4d6026a251666
Abinash <dashabinash2002@gmail.com>
2024-06-22 06:53:26 +0530

    tessting registration- Postman

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -4,7 +4,7 @@ import fs from "fs";
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.LOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 
@@ -15,6 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         console.log("File is uploaded to cloudinary",response.url)
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath) //remove the locally saved temporary file
